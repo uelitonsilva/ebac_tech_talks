e385199ec0edbc4d4041c814a99374491bc36fab
ueliton silva <ueliton45@hotmail.com>
2024-05-03 21:43:11 -0300

    imagens

---
+++ dist/index.8732be67.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("Jun 24, 2024 19:00:00").getTime(),t=setInterval(function(){let n=e-new Date().getTime();document.getElementById("contador").innerHTML=`${Math.floor(n/864e5)}d ${Math.floor(n%864e5/36e5)}h ${Math.floor(n%36e5/6e4)}m ${Math.floor(n%6e4/1e3)}s`,n<0&&(clearInterval(t),document.getElementById("contador").innerHTML="Esse Evento expirou")},1e3);
+//# sourceMappingURL=index.8732be67.js.map
